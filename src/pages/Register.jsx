bed1df499bf70ee32502f07770db02c68968c885
RonaldoFidelis <ronaldofidelis.ti@gmail.com>
2023-11-06 01:53:54 -0300

    Feat: Login and registration screen

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,25 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { Link } from 'react-router-dom';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCQ_PlrZP-U-vkrqGSGnT5M-biMmTmrs_A",
-  authDomain: "app-finance-33f60.firebaseapp.com",
-  projectId: "app-finance-33f60",
-  storageBucket: "app-finance-33f60.appspot.com",
-  messagingSenderId: "199612082398",
-  appId: "1:199612082398:web:69f710ea75a25b95dffa4f",
-  measurementId: "G-TZ2NGP2C80"
-};
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-
-export function Login() {
+export function Register() {
 
   const handleSubmit = () => { }
 
@@ -30,7 +11,7 @@ export function Login() {
         className="flex flex-col gap-5 bg-bg-display-login p-8 rounded-md"
         onSubmit={() => handleSubmit()}>
         <h1 
-          className="m-auto text-lg font-semibold">User login</h1>
+          className="m-auto text-lg font-semibold">Register User</h1>
         <label
           htmlFor="email"
           className="flex flex-col">
@@ -51,11 +32,22 @@ export function Login() {
             required
             className="h-[40px] rounded-sm bg-bg-input border-none outline-none p-2 text-slate-500" />
         </label>
+
+        <label
+          htmlFor="confirmPassword"
+          className="flex flex-col">
+          <p className="text-[15px] font-medium">Confirm Password</p>
+          <input
+            type="password"
+            id="confirmPassword"
+            required
+            className="h-[40px] rounded-sm bg-bg-input border-none outline-none p-2 text-slate-500" />
+        </label>
         <Link 
-          to='/register'
-          className="flex justify-end text-sm hover:text-font-main-login duration-500 text-slate-500">Don&apos;t have access?</Link>
+          to='/'
+          className="flex justify-end text-sm hover:text-font-main-login duration-500 text-slate-500">You have access?</Link>
         <button 
-          className="bg-btn-login h-[40px] rounded-sm font-medium hover:bg-btn-login-confirm duration-500">Login</button>
+          className="bg-btn-login h-[40px] rounded-sm font-medium hover:bg-btn-login-confirm duration-500">Register</button>
       </form>
     </div>
   )
