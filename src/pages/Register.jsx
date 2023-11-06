d8f72ee6328a30e94a3256a2d4ab731e8edeff7c
RonaldoFidelis <ronaldofidelis.ti@gmail.com>
2023-11-06 11:22:12 -0300

    Feature: Initial  logic at screen register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,50 @@
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { auth } from '../service/firebaseConfig';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 export function Register() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [classError, setClassError] = useState('border-none outline-none');
+  const [controlPassword, setControlPassword] = useState(false);
+  const [
+    createUserWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useCreateUserWithEmailAndPassword(auth);
 
-  const handleSubmit = () => { }
+  const handleSubmit = () => {
+    if(controlPassword){
+      createUserWithEmailAndPassword(email, password);
+    }else{
+      window.alert('Senhas diferentes');
+      return;
+    }
+  }
+
+  useEffect(() => {
+    let style = '';
+
+    if (confirmPassword == password) {
+      style = 'border-2 outline-none border-green-500'
+      setControlPassword(true)
+    } else {
+      style = 'border-2 outline-none border-red-500'
+      setControlPassword(false)
+    }
+    setClassError(style);
+  }, [confirmPassword, password])
+
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-bg-login text-font-main-login">
       <form
-        action=""
         className="flex flex-col gap-5 bg-bg-display-login p-8 rounded-md"
         onSubmit={() => handleSubmit()}>
-        <h1 
+        <h1
           className="m-auto text-lg font-semibold">Register User</h1>
         <label
           htmlFor="email"
@@ -20,6 +54,8 @@ export function Register() {
             type="email"
             id="email"
             required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             className="h-[40px] rounded-sm bg-bg-input border-none outline-none p-2 text-slate-500" />
         </label>
         <label
@@ -30,6 +66,8 @@ export function Register() {
             type="password"
             id="password"
             required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             className="h-[40px] rounded-sm bg-bg-input border-none outline-none p-2 text-slate-500" />
         </label>
 
@@ -41,12 +79,14 @@ export function Register() {
             type="password"
             id="confirmPassword"
             required
-            className="h-[40px] rounded-sm bg-bg-input border-none outline-none p-2 text-slate-500" />
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            className={`h-[40px] rounded-sm bg-bg-input p-2 text-slate-500 ${classError}`} />
         </label>
-        <Link 
+        <Link
           to='/'
           className="flex justify-end text-sm hover:text-font-main-login duration-500 text-slate-500">You have access?</Link>
-        <button 
+        <button
           className="bg-btn-login h-[40px] rounded-sm font-medium hover:bg-btn-login-confirm duration-500">Register</button>
       </form>
     </div>
